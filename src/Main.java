be663b8fba4575c9d0d3f82ee297b45a4e37fa72
Anastasia Propuskova <propuskova.aa@mail.ru>
2025-04-25 22:05:29 +0300

    adding operations with numbers

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Случайное число от 0 до 1: " + Math.random());
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        //сложение
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма двух чисел = " + sum);
+        //вычитание
+        int sub = firstNumber - secondNumber;
+        System.out.println("Разность двух чисел = " + sub);
+        //умножение
+        int multi = firstNumber * secondNumber;
+        System.out.println("Произведение двух чисел = " + multi);
+        //деление - частное
+        double div = (double) firstNumber/secondNumber;
+        if (secondNumber != 0) {
+            System.out.println("Деление двух чисел = " + div);
+        } else System.out.println("Ошибка! Делить на 0 нельзя.");
+
     }
 }
